b9715a203364877a4ffae6e7cb3a1dff59ede6d6
EndyKaufman <admin@site15.ru>
2024-08-23 00:46:21 +0400

    feat: Manual deployment of NestJS and Angular applications on a dedicated server via "Docker Compose" and "PM2" (2024-08-22)

--- apps/client-e2e/playwright.config.ts
+++ apps/client-e2e/playwright.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
-import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
@@ -16,19 +15,13 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60,
   ...nxE2EPreset(__filename, { testDir: './src' }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npx nx serve client',
-    url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
-    cwd: workspaceRoot,
   },
   projects: [
     {
@@ -45,25 +38,5 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    // Uncomment for mobile browsers support
-    /* {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    }, */
-
-    // Uncomment for branded browsers
-    /* {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    } */
   ],
 });
--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { createNestModule, NestModuleCategory } from '@nestjs-mod/common';
 
 import { PrismaModule } from '@nestjs-mod/prisma';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { join } from 'path';
 
 export const { AppModule } = createNestModule({
   moduleName: 'AppModule',
   moduleCategory: NestModuleCategory.feature,
   imports: [
     PrismaModule.forFeature({ featureModuleName: 'app' }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'assets', 'client'),
-    })
+    ...(process.env.DISABLE_SERVE_STATIC ? [] : [ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'browser'),
+    })])
   ],
   controllers: [AppController],
   providers: [AppService],
