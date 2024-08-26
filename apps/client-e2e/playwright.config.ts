bd19ecfe42ce949ee1d5bdf062a859f86c6619a9
EndyKaufman <admin@site15.ru>
2024-08-26 20:00:20 +0400

    fix: disable check ssl

--- apps/client-e2e/playwright.config.ts
+++ apps/client-e2e/playwright.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
   },
   projects: [
     {
