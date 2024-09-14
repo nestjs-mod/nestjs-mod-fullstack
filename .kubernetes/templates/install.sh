4ae63daf395456766ec73b40c3c39b0628aa5186
EndyKaufman <admin@site15.ru>
2024-09-14 12:10:02 +0400

    feat: try add k8s #11

--- .kubernetes/templates/install.sh
+++ .kubernetes/templates/install.sh
@@ -2,15 +2,15 @@
 set -e
 
 # docker regcred for pull docker images
-sudo microk8s kubectl delete secret docker-regcred 2>&1 >/dev/null & || echo 'not need delete secret docker-regcred'
-sudo microk8s kubectl create secret docker-registry docker-regcred --docker-server=%DOCKER_SERVER% --docker-username=%DOCKER_USERNAME% --docker-password=%DOCKER_PASSWORD% --docker-email=docker-regcred 2>&1 >/dev/null &
+sudo microk8s kubectl delete secret docker-regcred > /dev/null 2>&1 & || echo 'not need delete secret docker-regcred'
+sudo microk8s kubectl create secret docker-registry docker-regcred --docker-server=%DOCKER_SERVER% --docker-username=%DOCKER_USERNAME% --docker-password=%DOCKER_PASSWORD% --docker-email=docker-regcred > /dev/null 2>&1 &
 
 # namespace and common config
-sudo microk8s kubectl apply -f .kubernetes/generated/node 2>&1 >/dev/null &
-sudo microk8s kubectl get secret docker-regcred -n default -o yaml 2>&1 >/dev/null & || sed s/"namespace: default"/"namespace: %NAMESPACE%"/ || microk8s kubectl apply -n %NAMESPACE% -f - 2>&1 >/dev/null & || echo 'not need update docker-regcred'
+sudo microk8s kubectl apply -f .kubernetes/generated/node > /dev/null 2>&1 &
+sudo microk8s kubectl get secret docker-regcred -n default -o yaml > /dev/null 2>&1 & || sed s/"namespace: default"/"namespace: %NAMESPACE%"/ || microk8s kubectl apply -n %NAMESPACE% -f - > /dev/null 2>&1 & || echo 'not need update docker-regcred'
 
 # server
-sudo microk8s kubectl apply -f .kubernetes/generated/server 2>&1 >/dev/null &
+sudo microk8s kubectl apply -f .kubernetes/generated/server > /dev/null 2>&1 &
 
 # client
-sudo microk8s kubectl apply -f .kubernetes/generated/client 2>&1 >/dev/null &
+sudo microk8s kubectl apply -f .kubernetes/generated/client > /dev/null 2>&1 &
