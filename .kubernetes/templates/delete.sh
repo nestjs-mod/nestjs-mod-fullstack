0b0bb98502869f0c83187a75121a419a005732f5
EndyKaufman <admin@site15.ru>
2024-09-14 10:50:58 +0400

    feat: try add k8s #5

--- .kubernetes/templates/delete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# server
-microk8s kubectl delete -f ./server | echo 'not need delete server'
-
-# client
-microk8s kubectl delete -f ./server | echo 'not need delete client'
--- .kubernetes/templates/install.sh
+++ .kubernetes/templates/install.sh
@@ -6,11 +6,11 @@ microk8s kubectl delete secret docker-regcred || echo 'not need delete secret do
 microk8s kubectl create secret docker-registry docker-regcred --docker-server=%DOCKER_SERVER% --docker-username=%DOCKER_USERNAME% --docker-password=%DOCKER_PASSWORD% --docker-email=docker-regcred
 
 # namespace and common config
-microk8s kubectl apply -f ./node
+microk8s kubectl apply -f .kubernetes/generated/node
 microk8s kubectl get secret docker-regcred -n default -o yaml || sed s/"namespace: default"/"namespace: %NAMESPACE%"/ || microk8s kubectl apply -n %NAMESPACE% -f - || echo 'not need update docker-regcred'
 
 # server
-microk8s kubectl apply -f ./server
+microk8s kubectl apply -f .kubernetes/generated/server
 
 # client
-microk8s kubectl apply -f ./client
+microk8s kubectl apply -f .kubernetes/generated/client
