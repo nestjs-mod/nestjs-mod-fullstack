d22473e9322db2c7116913947340892a88c7f23f
EndyKaufman <admin@site15.ru>
2024-09-17 16:22:58 +0400

    chore: append exit to playwright tests after first fail and add check exists and renned our domain [skip e2e cache]

--- apps/client-e2e/playwright.config.ts
+++ apps/client-e2e/playwright.config.ts
@@ -14,6 +14,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  maxFailures: 1,
   timeout: 60 * 1000,
   ...nxE2EPreset(__filename, { testDir: './src' }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
