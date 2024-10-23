1b77dcbf37ad2a1fc29699a2490fa11a3422cc03
EndyKaufman <admin@site15.ru>
2024-10-23 21:43:43 +0400

    feat: add frontend for webhook

--- libs/common-angular/src/index.ts
+++ libs/common-angular/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/form-utils/get-formly-field-configs-from-type';
-export * from './lib/form-utils/get-formly-models-from-type';
 export * from './lib/form-utils/ng-changes';
 export * from './lib/grid-utils/get-query-meta-by-params';
 export * from './lib/grid-utils/query-meta';
