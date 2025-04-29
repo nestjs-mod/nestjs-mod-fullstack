be502bd96d7e099cccd54fc1a03ea3fc1077e263
EndyKaufman <admin@site15.ru>
2025-04-29 15:40:24 +0400

    chore: try use personal access token for pull docker images

--- .kubernetes/templates/install.sh
+++ .kubernetes/templates/install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-# docker regcred for pull docker images
-sudo microk8s kubectl delete secret docker-regcred || echo 'not need delete secret docker-regcred'
-sudo microk8s kubectl create secret docker-registry docker-regcred --docker-server=%DOCKER_SERVER% --docker-username=%DOCKER_USERNAME% --docker-password=%DOCKER_PASSWORD% --docker-email=docker-regcred
-
 # namespace and common config
 sudo microk8s kubectl apply -f .kubernetes/generated/node
-sudo microk8s kubectl get secret docker-regcred -n default -o yaml || sed s/"namespace: default"/"namespace: %NAMESPACE%"/ || microk8s kubectl apply -n %NAMESPACE% -f - || echo 'not need update docker-regcred'
+
+# docker regcred for pull docker images
+sudo microk8s kubectl delete secret -n %NAMESPACE% docker-regcred || echo 'not need delete secret docker-regcred'
+sudo microk8s kubectl create secret -n %NAMESPACE% docker-registry docker-regcred --docker-server=%DOCKER_SERVER% --docker-username=%DOCKER_USERNAME% --docker-password=%DOCKER_PASSWORD% --docker-email=docker-regcred
 
 # authorizer
 sudo microk8s kubectl delete -f .kubernetes/templates/authorizer/1.configmap.yaml || echo 'not need delete configmap'
